5eac6233e40d0bbfaa58c0abd26f5616dd18d80c
JoshuaF03 <joshuaf03@icloud.com>
2020-02-13 09:39:16 -0500

    Create index.php
    Created home page and basic description.

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE php>
+<html>
+  <head>
+    <title>Home - Josh's Site</title>
+  </head>
+  <body>
+    <h1>What is this site?</h1>
+    <p>
+      This site will be for anything I feel that I want here.
+      This may be serious things (Resume, CV, etc.), games, or just anything else I may decide belongs here.
+      Additionally, I hope to expand this site into something that will also be a test environment for any projects I am working on.
+    </p>
+  </body>
+</html>
