6e9ccc8db0072da71fda2d35a459c6cbc56664d0
JoshuaF03 <joshuaf03@icloud.com>
2020-02-14 07:31:10 -0500

    Added games.html
    Page for games has been added and started baisc circle game (learning jQuery)

--- testSite.js
+++ testSite.js
@@ -1,3 +1,17 @@
 $(document).ready(function() {
   $("#menuBar").load("./menuBar.html");
 });
+
+function getValueGame() {
+  var circleNo = document.getElementById('circleNo').value;
+  if (Math.floor(circleNo) == circleNo && $.isNumeric(circleNo) && circleNo > 0) {
+    randomizeGame(circleNo)
+  } else {
+    $("#circleError").load("./error.html");
+    $("#errorMsg").append("ERROR: Nice try! The number must be a positive integer!)";
+    }
+  }
+
+  function randomizeGame(value) {
+    for (i = 0; i < value;)
+  }
